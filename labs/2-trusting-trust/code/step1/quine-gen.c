ab4bf1123453da53b2144c2f9e02c9c969f60877
Guo Mingfei <1800012773@pku.edu.cn>
2024-01-11 18:54:28 -0800

    step1

--- labs/2-trusting-trust/code/step1/quine-gen.c
+++ labs/2-trusting-trust/code/step1/quine-gen.c
@@ -2,8 +2,29 @@
 // '\n' = 10) and spit out the <prog> array used in Figure 1 in
 // Thompson's paper.
 #include <stdio.h>
-
-int main(void) { 
+int main(void) {
     // put your code here.
-	return 0;
+    printf("char prog[] = {\n");
+    int global_i = 0;
+    char program[1000];
+    char c; int ret;
+    while (((ret = scanf("%c", &c)) == 1)) {
+        program[global_i] = c;
+        if (global_i % 8 != 0){
+            printf(", ");
+        }
+        global_i++;
+        printf("\t%d", c);
+        if (global_i % 8 == 0){
+            printf(",\n");
+        }
+    }
+
+    printf("0 };\n");
+    printf("%s", program);
+    return 0;
+
+
+    
+	// return 0;
 }
