1acb78ef3dad8425eb6a7021e72f4b95e9b119ff
Guo Mingfei <1800012773@pku.edu.cn>
2024-01-18 18:05:20 -0800

    lab4

---
+++ labs/4-cross-checking/1-fake-pi/clean-reboot.d
@@ -0,0 +1,8 @@
+clean-reboot.o: \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi//staff-src/clean-reboot.c \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h \
+  fake-pi.h \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h
---
+++ labs/4-cross-checking/1-fake-pi/putchar.d
@@ -0,0 +1,8 @@
+putchar.o: \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi//libc/putchar.c \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h \
+  fake-pi.h \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h \
+  /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h
