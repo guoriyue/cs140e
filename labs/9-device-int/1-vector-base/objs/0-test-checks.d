473dafd7a107ec49efef4ba20ff051fa55229922
Guo Mingfei <1800012773@pku.edu.cn>
2024-02-06 19:13:50 -0800

    part1 success

---
+++ labs/9-device-int/1-vector-base/objs/0-test-checks.d
@@ -0,0 +1,25 @@
+objs/0-test-checks.o: 0-test-checks.c \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h \
+ vector-base.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/bit-support.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/asm-helpers.h
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h:
+
+vector-base.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/bit-support.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/asm-helpers.h:
---
+++ labs/9-device-int/1-vector-base/objs/1-test-run.d
@@ -0,0 +1,34 @@
+objs/1-test-run.o: 1-test-run.c \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-interrupts.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h \
+ vector-base.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/bit-support.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/asm-helpers.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/cycle-count.h
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-interrupts.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h:
+
+vector-base.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/bit-support.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/asm-helpers.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/cycle-count.h:
