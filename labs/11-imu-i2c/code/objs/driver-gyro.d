558bcc4fbe68db294857b0604183eea7e1f7806f
Guo Mingfei <1800012773@pku.edu.cn>
2024-02-13 19:39:00 -0800

    done

---
+++ labs/11-imu-i2c/code/objs/driver-gyro.d
@@ -0,0 +1,24 @@
+objs/driver-gyro.o: driver-gyro.c \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h \
+ mpu-6050.h i2c.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/bit-support.h
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h:
+
+mpu-6050.h:
+
+i2c.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/bit-support.h:
