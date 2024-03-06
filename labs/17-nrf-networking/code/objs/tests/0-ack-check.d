2f87b8ecd67fd5bb0ce94162c247dcc2bc57946b
Guo Mingfei <1800012773@pku.edu.cn>
2024-03-05 18:17:20 -0800

    archive

---
+++ labs/17-nrf-networking/code/objs/tests/0-ack-check.d
@@ -0,0 +1,38 @@
+objs/tests/0-ack-check.o: tests/0-ack-check.c nrf-test.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h \
+ nrf.h /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/spi.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/circular.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-interrupts.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-inline-asm.h \
+ nrf-default-values.h
+
+nrf-test.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h:
+
+nrf.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/spi.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/circular.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-interrupts.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-inline-asm.h:
+
+nrf-default-values.h:
